1389315b11aba81c3320e1e4b7552468f8a22bb0
Štefan Földesi <stefan.foldesi@outlook.com>
2022-04-20 19:48:35 +0200

    Redirections

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 import http
+from flask import url_for
 from flask.wrappers import Response
 
 
@@ -7,6 +8,10 @@ def respond(status: str) -> Response:
         status_code = int(status)
         if status_code not in list(http.HTTPStatus):
             raise ValueError
-        return Response("", status_code)
+        response = Response("", status_code)
+        if 300 <= status_code < 400:
+            print("setting status")
+            response.headers["Location"] = url_for("respond", status=200)
+        return response
     except ValueError:
         return Response("", 404)
