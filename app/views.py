12d4b3f92de4565043ad58f18f6d82da86364a8f
Štefan Földesi <stefan.foldesi@outlook.com>
2022-04-20 19:26:56 +0200

    HTTP status validation

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
+import http
 from flask.wrappers import Response
 
 
 def respond(status: str) -> Response:
     try:
         status_code = int(status)
+        if status_code not in list(http.HTTPStatus):
+            raise ValueError
         return Response("", status_code)
     except ValueError:
         return Response("", 404)
